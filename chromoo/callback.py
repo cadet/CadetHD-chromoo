ea5d589fa6c0840a9ffa84868b8395b68251aa8d
Jayghosh Rao <j.rao@fz-juelich.de>
2021-12-22 16:27:58 +0100

    Major changes: use asyncio in callback

--- chromoo/callback.py
+++ chromoo/callback.py
@@ -7,6 +7,8 @@ import numpy as np
 import csv
 import os
 
+import asyncio
+
 class ChromooCallback(Callback):
 
     def __init__(self, cache) -> None:
@@ -19,30 +21,22 @@ class ChromooCallback(Callback):
             pass
 
     def notify(self, algorithm):
-        # NOTE: Only captures the first of many best solutions for MOOs
-        f_opt0 = algorithm.opt[0].F
-        x_opt0 = algorithm.opt[0].X
+        self.algorithm = algorithm
+        asyncio.run(self.subcallback())
 
-        F = algorithm.pop.get("F")
-        X = algorithm.pop.get("X")
+    async def subcallback(self):
+        await asyncio.gather(
+            asyncio.to_thread(self.save_last_best),
+            asyncio.to_thread(self.plot_best_scores),
+            asyncio.to_thread(self.plot_best_score_magnitude),
+            asyncio.to_thread(self.update_scatter_plot),
+            asyncio.to_thread(self.write_pareto)
+        )
 
-        # best_score_magnitude = sqrt(sum(map(lambda x: x**2, algorithm.opt[0].F)))
 
-        obj_magnitudes = self.magnitude(algorithm.opt)
-        best_score_magnitude = obj_magnitudes[0]
-        self.cache.best_scores.append(f_opt0)
-        self.cache.best_score_magnitude_pareto0.append(best_score_magnitude)
-        self.cache.last_best_individual = x_opt0
-
-        self.update_cache(X, F)
-        self.save_last_best()
-
-        with open('pareto.csv', 'w') as fp:
-            writer = csv.writer(fp)
-            writer.writerow(self.cache.p_names + self.cache.o_names)
-            writer.writerows(map(lambda opt: np.append(opt.X, opt.F) ,algorithm.opt))
-
-    def update_cache(self, X, F):
+    def update_scatter_plot(self):
+        F = self.algorithm.pop.get("F")
+        X = self.algorithm.pop.get("X")
         self.cache.add(X,F)
         self.cache.scatter_all(
             title=f"gen_all",
@@ -50,12 +44,29 @@ class ChromooCallback(Callback):
             yscale='log',
         )
 
-        self.cache.plot_best_scores()
-        self.cache.plot_best_score_magnitude()
-
     def save_last_best(self):
+        x_opt0 = self.algorithm.opt[0].X
+        self.cache.last_best_individual = x_opt0
         run_sim(self.cache.last_best_individual, self.cache.simulation, self.cache.parameters, "last_best.h5", store=True)
 
-    def magnitude(self, algo_opt):
+    def plot_best_scores(self):
+        f_opt0 = self.algorithm.opt[0].F
+        self.cache.best_scores.append(f_opt0)
+        self.cache.plot_best_scores()
+
+    def plot_best_score_magnitude(self):
+        opt = self.algorithm.opt
+        obj_magnitudes = self.magnitude(opt)
+        best_score_magnitude = obj_magnitudes[0]
+        self.cache.best_score_magnitude_pareto0.append(best_score_magnitude)
+        self.cache.plot_best_score_magnitude()
+
+    def magnitude(self, algo_opts):
         """ return magnitudes of a list of vectors """
-        return [ sqrt(sum(map(lambda x: x**2, opt.F))) for opt in algo_opt ]
+        return [ sqrt(sum(map(lambda x: x**2, opt.F))) for opt in algo_opts ]
+
+    def write_pareto(self):
+        with open('pareto.csv', 'w') as fp:
+            writer = csv.writer(fp)
+            writer.writerow(self.cache.p_names + self.cache.o_names)
+            writer.writerows(map(lambda opt: np.append(opt.X, opt.F) , self.algorithm.opt))
