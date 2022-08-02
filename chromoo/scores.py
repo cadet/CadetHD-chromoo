725bc9b0e6d046ae4c8e1b5c685fb796c75e277c
Jayghosh Rao <j.rao@fz-juelich.de>
2022-08-02 13:31:16 +0200

    assertions in scores

--- chromoo/scores.py
+++ chromoo/scores.py
@@ -4,6 +4,7 @@ def sse(y0, y):
     """
         calculate the SSE given 2 vectors
     """
+    assert(len(y0) == len(y))
     return sum([(n1 - n2)**2 for n1, n2 in zip(y, y0)])
 
 def logsse(y0, y):
@@ -11,6 +12,7 @@ def logsse(y0, y):
         Calculate the log of SSE of given 2 vectors
     """
 
+    assert(len(y0) == len(y))
     return log10(sum([(n1 - n2)**2 for n1, n2 in zip(y, y0)]))
 
 scores_dict = {
